57bae3c81c1d1a5b0877149da31b4bd90d7cc526
jdrsn <jaderson@jaderson.com>
2022-10-10 01:49:51 +0200

    crawler bugs fixed

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,20 @@ RUN apt-get update && apt-get install -y \
     poppler-utils && \
     rm -rf /var/lib/apt/lists/*
 
+# install google chrome
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+RUN apt-get -y update
+RUN apt-get install -y google-chrome-stable
+
+# install chromedriver
+RUN apt-get install -yqq unzip
+RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+
+# set display port to avoid crash
+ENV DISPLAY=:99
+
 # Install PDF converter
 RUN wget --no-check-certificate https://dl.xpdfreader.com/xpdf-tools-linux-4.04.tar.gz && \
     tar -xvf xpdf-tools-linux-4.04.tar.gz && cp xpdf-tools-linux-4.04/bin64/pdftotext /usr/local/bin
@@ -26,6 +40,9 @@ COPY rest_api /home/user/rest_api/
 
 # Install package
 RUN pip install --upgrade pip
+
+# install selenium
+RUN pip install selenium==3.8.0
 RUN pip install --no-cache-dir .[docstores,crawler,preprocessing,ocr,ray]
 RUN pip install --no-cache-dir rest_api/
 RUN ls /home/user
--- rest_api/controller/crawler.py
+++ rest_api/controller/crawler.py
@@ -1,19 +1,15 @@
 from typing import Optional, List
 
 import json
-import shutil
-import uuid
-from pathlib import Path
 
-from fastapi import FastAPI, APIRouter, UploadFile, File, Form, HTTPException, Depends
+from fastapi import FastAPI, APIRouter, Form, HTTPException, Depends
 from pydantic import BaseModel
 from haystack import Pipeline
-from haystack.nodes import BaseConverter, PreProcessor
+from haystack.nodes import PreProcessor
 
 from rest_api.utils import get_app, get_pipelines
 from rest_api.config import FILE_UPLOAD_PATH
 from rest_api.controller.utils import as_form
-
 
 router = APIRouter()
 app: FastAPI = get_app()
@@ -37,10 +33,10 @@ class Response(BaseModel):
 
 @router.post("/crawl")
 def crawl(
-    urls: List[str],
-    # JSON serialized string
-    meta: Optional[str] = Form("null"),  # type: ignore
-    preprocessor_params: PreprocessorParams = Depends(PreprocessorParams.as_form),  # type: ignore
+        urls: List[str],
+        # JSON serialized string
+        meta: Optional[str] = Form("null"),  # type: ignore
+        preprocessor_params: PreprocessorParams = Depends(PreprocessorParams.as_form),  # type: ignore
 ):
     """
     You can use this endpoint to crawl urls for indexing
@@ -58,6 +54,6 @@ def crawl(
     params = {}
     for preprocessor in preprocessors:
         params[preprocessor.name] = preprocessor_params.dict()
-    params['crawler'] = {'urls': urls, 'return_documents': True, 'output_dir': FILE_UPLOAD_PATH}
+    params['Crawler'] = {'urls': urls, 'return_documents': True, 'output_dir': FILE_UPLOAD_PATH}
 
     crawling_pipeline.run(params=params, meta=meta_form)
