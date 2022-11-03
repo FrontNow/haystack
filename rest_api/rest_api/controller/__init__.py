c04badc314466e63be6232383e47795b76eaa23d
Daniel Wiebking <daniel.wiebking@frontnow.com>
2022-11-03 18:00:45 +0100

    Fix

--- rest_api/rest_api/controller/__init__.py
+++ rest_api/rest_api/controller/__init__.py
@@ -1,7 +1,2 @@
 # this import is required for the Custom Components to be registered
 from rest_api.pipeline import custom_component
-
-import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.info("Loading custom component")
