3772c8e40f2d4cf0a4edb3e9008d0dfa95eea6fc
Daniel Wiebking <daniel.wiebking@frontnow.com>
2022-11-09 13:34:57 +0100

    Added params

--- rest_api/rest_api/controller/file_upload.py
+++ rest_api/rest_api/controller/file_upload.py
@@ -36,6 +36,15 @@ class PreprocessorParams(BaseModel):
     split_overlap: Optional[int] = None
     split_respect_sentence_boundary: Optional[bool] = None
 
+@as_form
+class TranslatorDecisionParams(BaseModel):
+    default_output: Optional[int] = None
+
+@as_form
+class RetrieverDecisionParams(BaseModel):
+    default_output: Optional[int] = None
+
+
 
 class Response(BaseModel):
     file_id: str
@@ -51,6 +60,8 @@ def upload_file(
     meta: Optional[str] = Form("null"),  # type: ignore
     fileconverter_params: FileConverterParams = Depends(FileConverterParams.as_form),  # type: ignore
     preprocessor_params: PreprocessorParams = Depends(PreprocessorParams.as_form),  # type: ignore
+    translator_decision_params: TranslatorDecisionParams = Depends(TranslatorDecisionParams.as_form),  # type: ignore
+    retriever_decision_params: RetrieverDecisionParams = Depends(RetrieverDecisionParams.as_form),  # type: ignore
     debug: Optional[bool] = None, # type: ignore
 ):
     """
@@ -88,5 +99,8 @@ def upload_file(
         params[converter.name] = fileconverter_params.dict()
     for preprocessor in preprocessors:
         params[preprocessor.name] = preprocessor_params.dict()
+
+    params["TranslatorDecision"] = translator_decision_params.dict()
+    params["RetrieverDecision"] = retriever_decision_params.dict()
 
     return indexing_pipeline.run(file_paths=file_paths, meta=file_metas, params=params, debug=debug)
